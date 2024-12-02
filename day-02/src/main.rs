aff0b6075caa911e334c7d365bb266e0b09dd50d
Andrew Mellen <andrew_mellen@icloud.com>
2024-12-02 01:46:21 -0500

    day 2

---
+++ day-02/src/main.rs
@@ -0,0 +1,53 @@
+use std::{
+    fs::{self},
+    io::Error,
+};
+
+fn main() {
+    let levels = read_input("input.txt").expect("failed to read input");
+
+    println!("Part 1: {}", part_1(&levels));
+    println!("Part 2: {}", part_2(&levels));
+}
+
+fn read_input(path: &str) -> Result<Vec<Vec<i64>>, Error> {
+    Ok(fs::read_to_string(path)?
+        .lines()
+        .map(|s| {
+            s.split_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect())
+}
+
+fn floor_is_safe(floor: &[i64]) -> bool {
+    (floor.is_sorted_by(|a, b| a < b) || floor.is_sorted_by(|a, b| a > b))
+        && floor.windows(2).all(|window| {
+            let diff = (window[0] - window[1]).abs();
+            diff >= 1 && diff <= 3
+        })
+}
+
+fn part_1(levels: &Vec<Vec<i64>>) -> i64 {
+    levels.iter().fold(0, |acc, floor| {
+        acc + if floor_is_safe(floor) { 1 } else { 0 }
+    })
+}
+
+fn part_2(levels: &Vec<Vec<i64>>) -> i64 {
+    levels.iter().fold(0, |acc, floor| {
+        acc + if floor_is_safe(floor) {
+            1
+        } else {
+            if (0..floor.len()).any(|i| {
+                let floor_without_i = [&floor[0..i], &floor[i + 1..floor.len()]].concat();
+                floor_is_safe(&floor_without_i)
+            }) {
+                1
+            } else {
+                0
+            }
+        }
+    })
+}
