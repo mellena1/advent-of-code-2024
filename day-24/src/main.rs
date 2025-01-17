8bd62b38e181c0ce40e23538fda67d94569c9bbd
Andrew Mellen <andrew_mellen@icloud.com>
2025-01-16 23:34:36 -0500

    code is a mess but d24 done

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
     fs::read_to_string,
 };
 
@@ -19,9 +20,22 @@ fn part1(device: &mut Device) -> usize {
 }
 
 fn part2(device: &mut Device) -> String {
-    println!("{:?}", device.fix_instrs_for_adders(&mut vec![]));
+    let swaps = device
+        .fix_instrs_for_adders(&mut vec![])
+        .expect("must find valid swaps");
 
-    "".to_string()
+    let mut outs: Vec<_> = swaps
+        .iter()
+        .flat_map(|swap| {
+            [
+                device.instructions[swap.0].c.clone(),
+                device.instructions[swap.1].c.clone(),
+            ]
+        })
+        .collect();
+    outs.sort();
+
+    outs.join(",")
 }
 
 fn read_input(path: &str) -> Result<Device, std::io::Error> {
@@ -96,26 +110,23 @@ impl Device {
             return Some(swaps.to_vec());
         }
 
-        println!("{} {:?}", wrong_gates[0], swaps);
-
         let first_broken = wrong_gates[0].clone();
         let relevant_instrs = if let Ok(v) = self.find_instrs_used_for_z(&first_broken) {
             v
         } else {
             return None;
         };
-        println!("{:?}", relevant_instrs);
 
         for (i, _) in relevant_instrs {
             for j in 0..self.instructions.len() {
+                if i == j {
+                    continue;
+                }
                 let swap = (i, j);
 
                 self.swap_instructions(&swap);
 
-                if self.z_gate_is_correct_for_adder(
-                    &first_broken,
-                    &self.get_base_inputs_for_gate(&first_broken),
-                ) {
+                if self.z_gate_is_correct_for_adder(&first_broken) {
                     swaps.push(swap);
                     if let Some(ans) = self.fix_instrs_for_adders(swaps) {
                         return Some(ans);
@@ -131,59 +142,12 @@ impl Device {
         None
     }
 
-    fn get_z_gate_base_inputs(&self) -> HashMap<String, Vec<Instruction>> {
-        let mut map = HashMap::new();
-
+    fn get_all_z_gates(&self) -> Vec<String> {
         self.instructions
             .iter()
             .filter_map(|instr| {
                 if instr.c.starts_with("z") {
-                    Some(instr.c.to_string())
-                } else {
-                    None
-                }
-            })
-            .for_each(|z_gate| {
-                map.insert(z_gate.to_string(), self.get_base_inputs_for_gate(&z_gate));
-            });
-
-        map
-    }
-
-    fn get_base_inputs_for_gate(&self, gate: &str) -> Vec<Instruction> {
-        let mut queue = vec![gate.to_string()];
-        let mut instrs = vec![];
-        let mut visited = HashSet::new();
-
-        while let Some(v) = queue.pop() {
-            visited.insert(v.to_string());
-            if let Some(instr) = self.instructions.iter().find(|instr| instr.c == v) {
-                if (instr.a.starts_with("x") || instr.a.starts_with("y"))
-                    && (instr.b.starts_with("x") || instr.b.starts_with("y"))
-                {
-                    instrs.push(instr.clone());
-                } else {
-                    if !visited.contains(&instr.a) {
-                        queue.push(instr.a.to_string());
-                    }
-                    if !visited.contains(&instr.b) {
-                        queue.push(instr.b.to_string());
-                    }
-                }
-            }
-        }
-
-        instrs
-    }
-
-    fn find_wrong_z_gates_for_adder(&self) -> Vec<String> {
-        let z_gates_to_input_instrs = self.get_z_gate_base_inputs();
-
-        z_gates_to_input_instrs
-            .iter()
-            .filter_map(|(z, instrs)| {
-                if !self.z_gate_is_correct_for_adder(z, &instrs) {
-                    Some(z.to_string())
+                    Some(instr.c.clone())
                 } else {
                     None
                 }
@@ -191,23 +155,24 @@ impl Device {
             .collect()
     }
 
-    fn z_gate_is_correct_for_adder(&self, z: &str, instrs: &[Instruction]) -> bool {
-        let z_bit: usize = z[1..].parse().unwrap();
+    fn find_wrong_z_gates_for_adder(&self) -> Vec<String> {
+        let z_gates = self.get_all_z_gates();
 
-        if instrs.len() != z_bit + 1 {
-            return false;
-        }
+        z_gates
+            .iter()
+            .filter_map(|z| {
+                if !self.z_gate_is_correct_for_adder(z) {
+                    Some(z.clone())
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
 
-        let mut sorted_instrs = Vec::from(instrs);
-        sorted_instrs.sort_by(|a, b| (&a.a[1..]).cmp(&b.a[1..]));
-
-        sorted_instrs.iter().enumerate().all(|(i, instr)| {
-            if i == sorted_instrs.len() - 1 {
-                instr.gate == Gate::XOR && instr.a[1..].parse::<usize>().unwrap() == i
-            } else {
-                instr.gate == Gate::AND && instr.a[1..].parse::<usize>().unwrap() == i
-            }
-        })
+    fn z_gate_is_correct_for_adder(&self, z: &str) -> bool {
+        self.dfs_for_tree_of_instrs(&z, &mut HashSet::new())
+            .map_or(false, |tree| tree.is_full_adder(&z))
     }
 
     fn find_instrs_used_for_z(&self, z: &str) -> Result<Vec<(usize, Instruction)>, ()> {
@@ -235,6 +200,34 @@ impl Device {
         }
 
         Ok(instrs)
+    }
+
+    fn dfs_for_tree_of_instrs(
+        &self,
+        gate: &str,
+        visited: &mut HashSet<String>,
+    ) -> Option<Box<Node>> {
+        if visited.contains(gate) {
+            return None;
+        }
+        if let Some((_, instr)) = self.find_instr_for_output(&gate) {
+            visited.insert(gate.to_string());
+            Some(Box::new(Node {
+                val: instr.clone(),
+                left: self.dfs_for_tree_of_instrs(&instr.a, visited),
+                right: self.dfs_for_tree_of_instrs(&instr.b, visited),
+            }))
+        } else {
+            None
+        }
+    }
+
+    fn find_instr_for_output(&self, gate: &str) -> Option<(usize, Instruction)> {
+        self.instructions
+            .iter()
+            .enumerate()
+            .find(|(_, instr)| instr.c == gate)
+            .map(|(i, instr)| (i, instr.clone()))
     }
 
     fn graphviz(&self) -> String {
@@ -292,6 +285,12 @@ impl Instruction {
     }
 }
 
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}{}", self.a, self.gate, self.b)
+    }
+}
+
 impl From<&str> for Instruction {
     fn from(value: &str) -> Self {
         let without_arrow = value.replace("->", "");
@@ -313,6 +312,20 @@ enum Gate {
     XOR,
 }
 
+impl Display for Gate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Gate::AND => "&",
+                Gate::OR => "|",
+                Gate::XOR => "^",
+            }
+        )
+    }
+}
+
 impl From<&str> for Gate {
     fn from(value: &str) -> Self {
         match value {
@@ -320,6 +333,112 @@ impl From<&str> for Gate {
             "OR" => Self::OR,
             "XOR" => Self::XOR,
             _ => panic!("unknown gate"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    val: Instruction,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
+}
+
+impl Node {
+    // returns true if a valid full adder for the out bit
+    fn is_full_adder(&self, out: &str) -> bool {
+        if out == "z00" {
+            return self.is_xor_for_main_bits("z00");
+        } else if out == "z45" {
+            return self.is_correct_carry_gates("z45");
+        }
+
+        if self.left.is_none() || self.right.is_none() {
+            return false;
+        }
+        let left = self.left.as_ref().unwrap();
+        let right = self.right.as_ref().unwrap();
+        self.val.gate == Gate::XOR
+            && ((left.is_xor_for_main_bits(out) && right.is_correct_carry_gates(out))
+                || (right.is_xor_for_main_bits(out) && left.is_correct_carry_gates(out)))
+    }
+
+    // for z03, should have x03^y03
+    fn is_xor_for_main_bits(&self, out: &str) -> bool {
+        let needed_inputs = [out.replace("z", "x"), out.replace("z", "y")];
+        let mut actual_inputs = [self.val.a.clone(), self.val.b.clone()];
+        actual_inputs.sort();
+
+        self.val.gate == Gate::XOR && actual_inputs == needed_inputs
+    }
+
+    fn is_correct_carry_gates(&self, out: &str) -> bool {
+        if out == "z01" {
+            return self.is_and_of_prev_bit(out);
+        }
+
+        if self.val.gate != Gate::OR {
+            return false;
+        }
+
+        if self.left.is_none() || self.right.is_none() {
+            return false;
+        }
+
+        let left = self.left.as_ref().unwrap();
+        let right = self.right.as_ref().unwrap();
+
+        (left.is_and_of_prev_bit(out) && right.is_carry_input(out))
+            || (right.is_and_of_prev_bit(out) && left.is_carry_input(out))
+    }
+
+    fn is_carry_input(&self, out: &str) -> bool {
+        if self.left.is_none() || self.right.is_none() {
+            return false;
+        }
+
+        if self.val.gate != Gate::AND {
+            return false;
+        }
+
+        let left = self.left.as_ref().unwrap();
+        let right = self.right.as_ref().unwrap();
+
+        let n = out[1..].parse::<usize>().unwrap();
+        let next_bit = format!("z{:02}", n - 1);
+
+        (left.is_xor_for_main_bits(&next_bit) && right.is_correct_carry_gates(&next_bit))
+            || (right.is_xor_for_main_bits(&next_bit) && left.is_correct_carry_gates(&next_bit))
+    }
+
+    fn is_and_of_prev_bit(&self, out: &str) -> bool {
+        let n = out[1..].parse::<usize>();
+        if n.is_err() {
+            return false;
+        }
+
+        let prev_bit = n.unwrap() - 1;
+        let needed_bits = [format!("x{:02}", prev_bit), format!("y{:02}", prev_bit)];
+
+        let mut actual_inputs = [self.val.a.clone(), self.val.b.clone()];
+        actual_inputs.sort();
+
+        self.val.gate == Gate::AND && actual_inputs == needed_bits
+    }
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.left.is_some() && self.right.is_some() {
+            write!(
+                f,
+                "({}){}({})",
+                self.left.as_ref().unwrap(),
+                self.val.gate,
+                self.right.as_ref().unwrap()
+            )
+        } else {
+            write!(f, "{}", self.val)
         }
     }
 }
